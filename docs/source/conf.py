ebcc5b354f5b77c6e8d62e19b50a18cfbec5b9ec
Galyfray <galyfray@gmail.com>
2023-01-18 09:35:44 +0100

    cleaned-up things

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,36 +12,36 @@ import toml
 
 PY_PROJECT = toml.load("../../pyproject.toml")["tool"]["poetry"]
 
-project = PY_PROJECT["name"]
-version = PY_PROJECT["version"]
+project: str = PY_PROJECT["name"]
+version: str = PY_PROJECT["version"]
 author: list[str] = [a.split("<")[0].strip() for a in PY_PROJECT["authors"]]
 
 if len(author) > 3:
-    shorts = []
+    shorts: list[str] = []
     for a in author:
         first, last = a.split(" ")
         shorts.append(f"{first[0]}. {last}")
     author = shorts
 
-author = ", ".join(author)
+author: str = ", ".join(author)
 copyright = f"{date.today().year}, {author}"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 extensions = [
-    'sphinx.ext.napoleon',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode'
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode"
 ]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-# html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+# html_static_path = ["_static"]
 
 # -- Napoleon configuration --------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/extensions/napoleon.html#configuration
